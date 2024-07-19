9e16aa2df5426a9a49fddd390d1a5b66a1b4b163
Harshita Pandey <161598094+Harshitap0809@users.noreply.github.com>
2024-07-19 15:40:53 +0000

    container configuration steps :

---
+++ 01_basics/test.js
@@ -0,0 +1,5 @@
+//container configuration steps : (this process will by default install nodejs into your system)
+//file >view >command palatte >type 'container' >select 'codespaces:Add Dev Container Config Files...' >select 'Node.js & JavaScript' >Select(default) >OK >rebuilt now
+
+//demo RUN 
+console.log("harshita")
